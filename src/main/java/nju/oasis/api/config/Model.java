ffe36328ef01d3cbd9f8d91e5902b19b761f40ca
reverse-mA <anhao0429@163.com>
2020-11-09 16:21:47 +0800

    修改公共参数

--- src/main/java/nju/oasis/api/config/Model.java
+++ src/main/java/nju/oasis/api/config/Model.java
@@ -3,15 +3,17 @@ package nju.oasis.api.config;
 //公共常量
 public class Model {
 
-    public final static String noArticleException = "No ArticleVO Present!";
+    public final static String NOARTICLEEXCEPTION = "No ArticleESVO Present!";
 
-    public final static String ieee = "IEEE";
+    public final static String IEEE = "IEEE";
 
-    public final static String acm = "acm";
+    public final static String ACM = "acm";
 
-    public final static String elasticUri = "39.105.94.200:9200";
+    public final static String ELASTICURI = "39.105.94.200:9200";
 
-    public final static String elasticUsername = "elastic";
+    public final static String ELASTICUSERNAME = "elastic";
 
-    public final static String elasticPassword = "elastic.org147258369";
+    public final static String ELASTICPASSWORD = "elastic.org147258369";
+
+    public final static int maxAbstractLength = 700;
 }
