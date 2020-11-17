aae91817fe70c4419142a30ebc33e0a572c5f7f3
reverse-mA <anhao0429@163.com>
2020-11-17 11:25:09 +0800

    feat:增加作者基本信息获取最多合作者和最大引用文章

--- src/main/java/nju/oasis/api/serviceImpl/AuthorServiceImpl.java
+++ src/main/java/nju/oasis/api/serviceImpl/AuthorServiceImpl.java
@@ -3,14 +3,14 @@ package nju.oasis.api.serviceImpl;
 import lombok.extern.slf4j.Slf4j;
 import nju.oasis.api.dao.AuthorDAO;
 import nju.oasis.api.domain.AuthorES;
+import nju.oasis.api.domain.SimArticle;
 import nju.oasis.api.service.AuthorService;
-import nju.oasis.api.vo.AuthorESVO;
-import nju.oasis.api.vo.ResponseVO;
-import nju.oasis.api.vo.ResultCode;
+import nju.oasis.api.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -18,6 +18,9 @@ public class AuthorServiceImpl implements AuthorService {
 
     @Autowired
     AuthorDAO authorDAO;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public ResponseVO findById(String id){
         if(id == null){
@@ -28,8 +31,51 @@ public class AuthorServiceImpl implements AuthorService {
             log.warn("[findById] authorId = " + id + " is not present!" );
             return ResponseVO.output(ResultCode.PARAM_ERROR,null);
         }
+
         AuthorES authorES = authorESOptional.get();
-        return ResponseVO.output(ResultCode.SUCCESS,new AuthorESVO(authorES));
+        AuthorESVO authorESVO = new AuthorESVO(authorES);
+        long authorId = authorES.getAuthorId();
+        List<SimArticle> simArticleList = authorES.getAuthorArticle();
+        List<Long>articleIds = new ArrayList<>();
+        for(SimArticle simArticle:simArticleList){
+            articleIds.add(simArticle.getArticleId());
+        }
+        AuthorRequestForm authorRequestForm = new AuthorRequestForm();
+        authorRequestForm.setAuthorId(authorId);
+        authorRequestForm.setArticleIds(articleIds);
+        try {
+            ResponseVO responseVO = restTemplate.postForObject("http://OASIS-SERV/author/articles",
+                    authorRequestForm, ResponseVO.class);
+
+            if (responseVO.getStatus() != 0) {
+                log.warn("[findById] authorId: " + id + " no mostFrequentCoAuthor or mostCitedArticle found!");
+                return ResponseVO.output(ResultCode.SUCCESS, authorESVO);
+            }
+            Map<String, Object> result = (Map<String, Object>) responseVO.getData();
+
+            if (!result.containsKey("articleId") || !result.containsKey("articleName")) {
+                log.warn("[findById] authorId: " + id + " no mostCitedArticle found!");
+            }
+            else{
+                long articleId = Long.valueOf((int)result.get("articleId"));
+                String articleName = (String) result.get("articleName");
+                authorESVO.setMostCitedArticle(articleId,articleName);
+            }
+
+            if (!result.containsKey("coAuthorId")) {
+                log.warn("[findById] authorId: " + id + " no mostFrequentCoAuthor found!");
+            }
+            else{
+                long coAuthorId = Long.valueOf((int)result.get("coAuthorId"));
+                authorESVO.setMostFrequentCoauthor(coAuthorId);
+            }
+
+            return ResponseVO.output(ResultCode.SUCCESS, authorESVO);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            log.warn("[findById] error: " + ex.getMessage());
+            return ResponseVO.output(ResultCode.PARAM_ERROR,null);
+        }
 
     }
 }
---
+++ src/main/java/nju/oasis/api/vo/AuthorRequestForm.java
@@ -0,0 +1,19 @@
+package nju.oasis.api.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AuthorRequestForm {
+    /**
+     * author的id
+     */
+    private Long authorId;
+
+    /**
+     * author所有文章的id
+     */
+    private List<Long> articleIds;
+
+}
